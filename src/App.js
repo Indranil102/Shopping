607ad5e141ebba6063e53b06515ae8c0ebd0ca3d
Indranil102 <indranilsamanta2003@gmail.com>
2024-11-24 12:01:41 +0530

    Link file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import Navbar from './Components/Navbar/Navbar';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from './Pages/Shop'; // Corrected component name
+import ShopCategory from './Pages/ShopCategory';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
+import Product from './Pages/Product';
 
 function App() {
   return (
-    <div >
-      
-      <Navbar />
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          {/* Define routes for the app */}
+          <Route path="/" element={<Shop />} />
+          <Route path="/mens" element={<ShopCategory category="men" />} />
+          <Route path="/womens" element={<ShopCategory category="women" />} />
+          <Route path="/kids" element={<ShopCategory category="kid" />} />
+          <Route path="/product/:productId" element={<Product />} /> {/* Dynamic route */}
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
