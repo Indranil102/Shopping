f147a4a48f2389b4685a9738253d1eeb1e0a4d7e
Indranil102 <indranilsamanta2003@gmail.com>
2024-12-11 11:27:40 +0530

    productPage

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import './CSS/Loginsignup.css'
 
 const LoginSignup = () => {
   return (
-    <div>
-      
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Your name'  />
+          <input type="email" placeholder='password' />
+          <input type="password" placeholder='Password' />
+        </div>
+        <button>Continue</button>
+        <p className='loginsignup-login'>
+          Aready have an account ? <span>Login here</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name='' id='' />
+          <p>By continuing, I agree to the term of use & privacy policy.</p>
+        </div>
+      </div>
     </div>
   )
 }
